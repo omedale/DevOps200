d1773f02fa9ea6b17889be5444264b02aef9c253
oluwafemi medalem <oluwafemi.medale@andela.com>
2018-01-29 12:06:56 +0100

    First commit

---
+++ app/tests/app-test.py
@@ -0,0 +1,21 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+import unittest
+from src.app import my_function
+
+# print(sys.path,'SYS PATH')
+
+class MyTest(unittest.TestCase):
+    def test_my_function(self):
+        self.assertEqual(my_function(1, 1), 2)
+        self.assertEqual(my_function(1, -1), 0)
+        self.assertEqual(my_function(0, 0), 0)
+        self.assertEqual(my_function(-1, -1), -2)
+        self.assertEqual(my_function(1.0, 1), 2)        
+        self.assertEqual(my_function(1.1, 1.1), 2.2)        
+        
+        
+        
+if __name__ == '__main__':
+    unittest.main()
